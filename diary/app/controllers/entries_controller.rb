b44bc8a55c8ccc4e9b9d74fb29c40825111bf10c
Adhithyan <adhi@Adhithyans-MacBook-Pro.local>
2018-04-05 15:20:07 +0530

    diary all operations completed

--- diary/app/controllers/entries_controller.rb
+++ diary/app/controllers/entries_controller.rb
@@ -6,13 +6,23 @@ class EntriesController < ApplicationController
   def create
     @entry = Entry.new(diary_params)
 
-    if @entry.save?
+    if @entry.save
       redirect_to @entry
     else
       render 'new' #issue same request, the same article object is passed again
       #redirect_to new request is issued.
     end
 
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+
+    if @entry.update(diary_params)
+      redirect_to @entry
+    else
+      render 'edit'
+    end
   end
 
   def index
@@ -23,10 +33,18 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params[:id])
   end
 
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+
+    redirect_to entries_path
+  end
+  
   private
 
   def diary_params
     params.require(:entry).permit(:date, :note)
   end
+
 
 end
