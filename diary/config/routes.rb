3ebc6c0dcd6d37a5f27269101544a4c3a30b29f9
v-adhithyan <aavispeaks@gmail.com>
2018-04-06 15:04:17 +0530

    changes

--- diary/config/routes.rb
+++ diary/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :users
     resources :entries
-  root 'welcome#index'
+  root 'entries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
